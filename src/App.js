90741e6ea64f830ed8ae0c497b1e8c79ca2b051f
manicharanchindam <manicharanchindam@email.com>
2024-07-04 10:34:23 +0530

    added bug

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Provider } from "react-redux";
 import { store } from "./store/store";
 import Counter from "./components/usestatedemo";
 
-function App() {
+functions App() {
   return (
     <Provider store={store}>
       <Box sx={{ display: "flex" }}>
